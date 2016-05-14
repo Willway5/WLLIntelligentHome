af4716926e02f1ca68ba3ddff078c8645de3a40a
yuzhouheike <26108724@qq.com>
2016-05-14 15:54:10 +0800

    第一次提交测试下

---
+++ IntelligentHome/IntelligentHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntelligentHome
+//
+//  Created by ma c on 16/5/14.
+//  Copyright © 2016年 王磊磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntelligentHome/IntelligentHome/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  IntelligentHome
+//
+//  Created by ma c on 16/5/14.
+//  Copyright © 2016年 王磊磊. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
